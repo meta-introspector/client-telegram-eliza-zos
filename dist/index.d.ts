303152330036eee84f433003e19601742ec62732
Carlos V <carlosv.provoos@gmail.com>
2025-02-12 13:41:08 -0500

    chore: update plugin dist

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import * as _elizaos_core from '@elizaos/core';
+
+declare const telegramPlugin: {
+    name: string;
+    description: string;
+    clients: _elizaos_core.Client[];
+};
+
+export { telegramPlugin as default };
